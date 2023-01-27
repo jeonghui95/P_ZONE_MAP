6051f8dfc5eb8fa3a1c3b996ea0c9316c16b9b3f
smilemask92 <rhkwk3333@naver.com>
2023-01-27 11:29:40 +0900

    fix: Django secret 환경변수화

--- P_ZONE_MAP/P_ZONE_MAP/settings.py
+++ P_ZONE_MAP/P_ZONE_MAP/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ("DJNAGO_SECRET")
-SECRET_KEY = 'django-insecure-b2=-iul+ox15if-*tr6wz34vkjt=$l5i=s#$7u42e_b!76v9hi'
+SECRET_KEY = os.environ("DJNAGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
