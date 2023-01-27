63a9cb21d84c99167a44d40f61030ea146f78a27
smilemask92 <rhkwk3333@naver.com>
2023-01-27 20:43:43 +0900

    fix

--- P_ZONE_MAP/P_ZONE_MAP/settings.py
+++ P_ZONE_MAP/P_ZONE_MAP/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ("DJNAGO_SECRET")
+try:
+    SECRET_KEY = os.environ("DJNAGO_SECRET")
+except:
+    print("NULL")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
